326412faa6fc61706f99901c6bcc193ea6fe2ddf
Jeff Glanville <jeff.glanville@outlook.com>
2020-08-19 13:26:14 -0500

    update messages-router.js

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,24 +3,71 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+    try {
+        const messages = await db.select("*").from("messages")
+
+        res.json(messages)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const [message] = await db.select("*").from("messages").where("id", req.params.id).limit(1)
+
+        res.json(message)
+    }catch(err) {
+        next(err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const [id] = await db.insert({
+            title: req.body.title,
+            contents: req.body.contents
+        })
+        .into("messages")
+                               //replaces .select and .from, replaces .limit(1)
+        const message = await db("messages").where("id", id).first()
+
+        res.status(201).json(message)
+    }catch(err) {
+        next(err)
+    }
+})
+
+router.put("/:id", async (req, res, next) => {
+    try {
+        await db("messages")
+        .update({
+            title: req.body.title,
+            contents: req.body.contents
+        })
+        .where("id", req.params.id)
+
+        const message = await db("messages")
+        .where("id", req.params.id)
+        .first()
+
+        res.json(message)
+    }catch(err) {
+        next(err)
+    }
 
 })
 
-router.get("/:id", (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
+    try {
+       await db("messages").where("id", req.params.id)
+       .del()
 
-})
-
-router.post("/", (req, res, next) => {
-
-})
-
-router.put("/:id", (req, res, next) => {
-
-})
-
-router.delete("/:id", (req, res, next) => {
-
+        res.status(204).end()
+    } catch(err) {
+        next(err)
+    }
 })
 
 module.exports = router
